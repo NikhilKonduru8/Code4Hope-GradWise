7d420940924a370f22529c7283fda2fd69202609
NikhilKonduru8 <nikhilkonduru8@gmail.com>
2025-06-28 16:44:21 -0700

    Delete (MAIN)GradeWise College Prep Web APP Code/gradwise-mvp directory

--- (MAIN)GradeWise College Prep Web APP Code/gradwise-mvp/lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr"
-
-export function createClient() {
-  return createBrowserClient(
-    "https://edbkkednmottpactcngg.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkYmtrZWRubW90dHBhY3RjbmdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExMzQ0MDEsImV4cCI6MjA2NjcxMDQwMX0.CdNmfHw4ZYNVBM6lm-TTENmE7SiuHnt00P_CEibJ4oA",
-  )
-}
